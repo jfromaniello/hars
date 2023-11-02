4de6b59039c96bbf199b46517584272f5b00b2f3
José F. Romaniello <jfromaniello@gmail.com>
2023-11-02 15:27:45 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import { useCallback, useState } from 'react'
+import { sanitize } from 'har-sanitizer';
+import FileSaver from 'file-saver';
+import {useDropzone} from 'react-dropzone'
+
+import './App.css'
+
+function App() {
+  type SanitizationType = 'obfuscate' | 'hash';
+
+  const [cookieConfig, setCookieConfig ] = useState<SanitizationType>('obfuscate');
+  const [tokensConfig, setTokensConfig ] = useState<SanitizationType>('obfuscate');
+
+  const onDrop = useCallback(async (files: File[]) => {
+    if (!files || files.length === 0) return;
+    const file = files[0];
+    const content = new TextDecoder("utf-8").decode(await file.arrayBuffer());
+    const sanitized = await sanitize(JSON.parse(content), {
+      cookies: cookieConfig,
+      tokens: tokensConfig,
+    });
+    const blob = new Blob(
+      [JSON.stringify(sanitized, null, 2)],
+      { type: "text/plain;charset=utf-8" }
+    );
+    FileSaver.saveAs(blob, file.name.replace('.har', '-sanitized.har'));
+  }, [])
+
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <>
+      <h1>Har Sanitizer</h1>
+      <input {...getInputProps()} />
+      <hr />
+      <div {...getRootProps()} style={{ minWidth: 445 }}>
+        {
+          isDragActive ?
+            <p>Drop your HAR file to get it sanitized...</p> :
+            <p>Drop your HAR file to get it sanitized, or click to select files</p>
+        }
+      </div>
+      <hr />
+      <div style={{ textAlign: 'left'}}>
+        <ul>
+          <li>Cookies will be either <strong>obfuscated</strong> or <strong>hashed</strong>.</li>
+          <li>Tokens will be either <strong>obfuscated</strong> or <strong>hashed</strong>.</li>
+          <li>Passwords will be obfuscated.</li>
+        </ul>
+        <p>
+          Configuration:
+        </p>
+        <div>
+          Cookies:
+          <select onChange={e => setCookieConfig(e.target.value as SanitizationType)}>
+            <option value="obfuscate">obfuscate</option>
+            <option value="hash">hash</option>
+          </select>
+        </div>
+        <div>
+          Tokens:
+          <select onChange={e => setTokensConfig(e.target.value as SanitizationType)}>
+            <option value="obfuscate">obfuscate</option>
+            <option value="hash">hash</option>
+          </select>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
